e553c555dd2dc8728f259eb1b2be90b335569255
derwind <uncertainty_principle_ss@yahoo.co.jp>
2024-12-19 22:54:43 +0900

    Fix incorrect regex by reverting codes to the original ones

--- gtexfix/to.py
+++ gtexfix/to.py
@@ -35,7 +35,7 @@ def convert_to(filename, ignore_no_end_patterns=False):
     filebase = re.sub('.tex$','',filename)
 
     ### Search for possible token conflicts
-    conflicts=re.findall(r'\\[ *[012][\\.][0-9]+\\]',source)
+    conflicts=re.findall('\[ *[012][\.\,][0-9]+\]',source)
     if(conflicts!=[]):
         print('Token conflicts detected: ',conflicts)
         sys.exit('Tokens may overlap with the content. Change tokens or remove the source of conflict.')
